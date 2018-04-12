a5a91b2c07acb9c4fbccdaea576282aa51e60831
Sevih <38295644+Sevih@users.noreply.github.com>
2018-04-12 11:25:26 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
 
 bot.on('ready', () =>{
     console.log("Ok, prêt à basura des mères !!");
+    bot.user.setGame('Basura des mères');
 });
 
-bot.login('process.env.TOKEN')
+bot.login('NDMzOTE0MTgzNjEzMDIyMjE4.DbCxmg.cOd9Hduz7gZdz7zWczP1fC9OtJ4')
 
 bot.on('message', message => {
     if(message.content === "ping"){
